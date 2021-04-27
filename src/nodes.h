f4d8662e92ada5e6471e82cc394e8e51fcf3e9dd
AakankshaChhavi <47914669+AakankshaChhavi@users.noreply.github.com>
2021-04-28 02:59:52 +0530

    Update nodes.h

--- src/nodes.h
+++ src/nodes.h
@@ -6,28 +6,43 @@
 #include "3ac.h"
 
 using namespace std;
-typedef struct {
-   long long size;
-   long long int iVal;
-   long double rVal;
-   char* str;
-   char cVal;
-   int is_init;
-   int expr_type;
-   string node_name;
-   string node_type;
-   string node_key;
-   int node_id;
-   //3AC
-   qid place;
-   list<int> truelist;
-   list<int> nextlist;
-   list<int> falselist;
-   list<int> breaklist;
-   list<int> continuelist;
-   list<int> caselist;
-} node;
-
+void graphStart();
+void graphEnd();
+int ID_for_Node();
+class Node{
+        
+    public:
+        string node_name;
+        string node_type;
+        string node_key;
+        int node_id;
+        long long size;
+        long long int iVal;
+        long double rVal;
+        char* str;
+        char cVal;
+        int is_init;
+        int expr_type;
+        Node(char* str){
+            node_name = str;
+            node_id = ID_for_Node();
+        }
+        void mkleaf();
+        void mknode(char *op, Node *l, Node *r);
+        void mknode(Node *l,Node *m, Node *r);
+        void mknode(char *op1,char *op3, Node *l,char *op2);
+        void mknode(Node *a1,Node *a2, Node *a3, Node*a4,char* op);
+        void mknode(Node *a1,Node *a2, Node *a3, Node*a4,Node *a5);
+        void mknode(Node *a, int flag);
+        //3AC
+      qid place;
+      list<int> truelist;
+      list<int> nextlist;
+      list<int> falselist;
+      list<int> breaklist;
+      list<int> continuelist;
+      list<int> caselist;
+};
 enum ntype {
     N_INT , N_LONG , N_LONGLONG , N_FLOAT , N_DOUBLE, N_LONGDOUBLE
 };
@@ -44,17 +59,6 @@ typedef struct{
    char* str;
    char cVal;
    int expr_type;
-   node* nPtr;
+   Node* nPtr;
 } exprNode;
-
-int ID_for_Node();
-void graphStart();
-void graphEnd();
-node *mkleaf(char *str);
-node *mknode(char *str,char *op, node *l, node *r);
-node *mknode(char *str,node *l,node *m, node *r);
-node *mknode(char *str,char *op1,char *op3, node *l,char *op2);
-node *mknode(char *str,node *a1,node *a2, node *a3, node*a4,char* op);
-node *mknode(char *str,node *a1,node *a2, node *a3, node*a4,node *a5);
-node *mknode(char *str, node *a, int flag);
 
